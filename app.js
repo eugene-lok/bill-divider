c3fda2c038ad1665992b0f43d41c91431d6b62b9
Eugene Lok <eugeneywlok@gmail.com>
2020-07-29 22:20:39 -0600

    Add input field, declare controller modules
    - Added node_modules to .gitignore

---
+++ app.js
@@ -0,0 +1,21 @@
+// Bill Controller
+var billController = (function() {
+
+})();
+
+// UI Controller
+var uiController = (function() {
+
+})();
+
+var controller = (function(billCtrl, UICtrl) {
+
+    // Adds an expense to the bill
+    document.querySelector(".addExpense").addEventListener('click', function() {
+        // Get input from field 
+    });
+
+
+
+})(billController, UIController);
+
