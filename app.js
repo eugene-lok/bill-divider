8fa87c7efe423ea373bbc720775bf3e731132210
Eugene Lok <eugeneywlok@gmail.com>
2020-07-30 23:05:38 -0600

    Add event listeners

--- app.js
+++ app.js
@@ -10,12 +10,30 @@ var uiController = (function() {
 
 var controller = (function(billCtrl, UICtrl) {
 
-    // Adds an expense to the bill
-    document.querySelector(".addExpense").addEventListener('click', function() {
-        // Get input from field 
+
+    var ctrlAddExpense = function() {
+        // Get field input
+        // Add expense to bill controller
+        // Add expense to UI
+        // Calculate total owed
+        // Display on UI
+    }
+
+    // Adds an expense to the bill (Button)
+    document.querySelector(".btn-addExpense").addEventListener('click', ctrlAddExpense);
+
+    // Adds an expense to the bill (Enter key)
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13)  {
+            console.log("Enter was pressed");
+        }
+    });
+
+    // Adds a person to the group
+    document.querySelector(".btn-addPerson").addEventListener('click', function() {
+
     });
 
 
-
-})(billController, UIController);
+})(billController, uiController);
 
